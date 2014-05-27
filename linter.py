589399db8394477ef0b134b20205f8e84bc9135f
Alexander Aprelev <alexander@aprelev.com>
2014-05-27 00:41:13 -0400

    Moved cfserver.exe name to preferences

--- linter.py
+++ linter.py
@@ -14,7 +14,7 @@ import subprocess
 import os
 import threading
 import SublimeLinter
-from SublimeLinter.lint import Linter, util
+import sublime
 
 # from Default.exec import ProcessListener, AsyncProcess
 
@@ -54,43 +54,15 @@ class Cfserver(Linter):
     def __init__(self, view, syntax):
         Linter.__init__(self, view, syntax)
 
-        print("Initialized cfserver linter. [Re]starting cfserver")
-
-        startupinfo = None
-        if os.name == "nt":
-            startupinfo = subprocess.STARTUPINFO()
-            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-
-        cmd = r'C:\\Users\\alexander.APRELEV\\.IntelliJIdea13\\system\\plugins-sandbox\\plugins\\CppTools\\lib\\cfserver.exe'
-
-        print("Starting " + cmd)
-
-        self.cfserver = util.popen(cmd)
-
-        if self.cfserver.stdout:
-            threading.Thread(target=self.read_stdout).start()
-
-        if self.cfserver.stderr:
-            threading.Thread(target=self.read_stderr).start()
-
-        print("Started cfserver pid=%d" % (self.cfserver.pid))
-        self.write_stdin()
-
-    def poll(self):
-        return self.cfserver.poll() == None
-
-    def exit_code(self):
-        return self.cfserver.poll()
-
     def write_stdin(self):
-        self.cfserver.stdin.write(bytes(
+        Cfserver.cfserver.stdin.write(bytes(
 r'''
-source "c:\\Users\\alexander.APRELEV\\.IntelliJIdea13\\system\\plugins-sandbox\\plugins\\CppTools\\lib\\profile.tcl"
+source "C:\\Users\\alexander.APRELEV\\Downloads\\CppTools\\CppTools\\lib\\profile.tcl"
 # version 0.8.3 (Win32) build no. 1 on Feb 28 2012 14:45:38
 # Command: "C:/Users/alexander.APRELEV/.IntelliJIdea13/system/plugins-sandbox/plugins/CppTools/lib/cfserver.exe" --idea --catchexceptions --inLogName C:\\Users\\alexander.APRELEV\\.IntelliJIdea13\\system\\plugins-sandbox\\plugins\\CppTools\\logs\\untitled\in --outLogName C:\\Users\\alexander.APRELEV\\.IntelliJIdea13\\system\\plugins-sandbox\\plugins\\CppTools\\logs\\untitled\\out
 # Library: $Id: lib.tcl, 2010/11/26 14:27 shd Exp $
 #timer: 0.003
-source "C:\\Users\\alexander.APRELEV\\.IntelliJIdea13\\system\\plugins-sandbox\\plugins\\CppTools\\lib\\lib.tcl"
+source "C:\\Users\\alexander.APRELEV\\Downloads\\CppTools\\CppTools\\lib\\lib.tcl"
 #timer: 0.004
 use-abs-path
 begin-config cmode
@@ -100,34 +72,75 @@ user-source-root "C:/Users/alexander.APRELEV/IdeaProjects/untitled"
 define WIN32 "1"
 '''
             , "ascii"))
-        self.cfserver.stdin.flush()
+        Cfserver.cfserver.stdin.flush()
 
     def read_stdout(self):
         while True:
-            data = os.read(self.cfserver.stdout.fileno(), 2**15)
+            data = os.read(Cfserver.cfserver.stdout.fileno(), 2**15)
 
             if len(data) > 0:
-                if self.cfserverListener:
-                    self.cfserverListener.on_data(self, data)
+                if Cfserver.cfserverListener:
+                    Cfserver.cfserverListener.on_data(self, data)
             else:
-                self.cfserver.stdout.close()
-                if self.cfserverListener:
-                    self.cfserverListener.on_finished(self)
+                Cfserver.cfserver.stdout.close()
+                if Cfserver.cfserverListener:
+                    Cfserver.cfserverListener.on_finished(self)
                 break
 
     def read_stderr(self):
         while True:
-            data = os.read(self.cfserver.stderr.fileno(), 2**15)
+            data = os.read(Cfserver.cfserver.stderr.fileno(), 2**15)
 
             if len(data) > 0:
-                if self.cfserverListener:
-                    self.cfserverListener.on_data(self, data)
+                if Cfserver.cfserverListener:
+                    Cfserver.cfserverListener.on_data(self, data)
             else:
-                self.cfserver.stderr.close()
+                Cfserver.cfserver.stderr.close()
                 break
+
+    def get_settings():
+        return sublime.load_settings("SublimeLinter-contrib-cfserver.sublime-settings")
+
+
+    def get_setting(key, default=None, view=None):
+        try:
+            if view == None:
+                view = sublime.active_window().active_view()
+            s = view.settings()
+            if s.has("sublimecontribcfserver_%s" % key):
+                return s.get("sublimecontribcfserver_%s" % key)
+        except:
+            pass
+        return Cfserver.get_settings().get(key, default)
 
     def run(self, cmd, code):
         print("cfserver")
+
+        print("Initialized cfserver linter. [Re]starting cfserver = %s" % Cfserver.cfserver)
+        if Cfserver.cfserver != None and Cfserver.cfserver.poll() == None:
+            print("Cfserver already running with pid %d" % (Cfserver.cfserver.pid))
+        else:
+            startupinfo = None
+            if os.name == "nt":
+                startupinfo = subprocess.STARTUPINFO()
+                startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+            cmd = Cfserver.get_setting("cfserver_path", "cfserver.exe")
+
+            print("Starting " + cmd)
+
+            Cfserver.cfserver = util.popen(cmd)
+
+            if Cfserver.cfserver.stdout:
+                threading.Thread(target=self.read_stdout).start()
+
+            if Cfserver.cfserver.stderr:
+                threading.Thread(target=self.read_stderr).start()
+
+            print("Started cfserver pid=%d" % (Cfserver.cfserver.pid))
+        self.write_stdin()
+
+
 #        print("cfserver linter %s run cmd=%s, code=%s" % (self, cmd, code))
         return (
             r'ERRORS "c:\\Users\\alexander.APRELEV\\IdeaProjects\\untitled\\test.cpp" 0\n'
